89ebe524fa9e6106bb129cfb35a0a44a9c79dac7
shariyer <s.m.shariyer04@gmail.com>
2022-11-08 14:44:24 +0600

    vercel mongodb dotenv added and middle ware setup done

--- index.js
+++ index.js
@@ -3,6 +3,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+// middleware setUp
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome!! S.M.'S Snap's SERVER IS RUNNING");
