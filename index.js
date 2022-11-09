43e2d751f05b686bb558797bb86ef9af1fff66bc
shariyer <s.m.shariyer04@gmail.com>
2022-11-09 13:49:41 +0600

    /services api created from database

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const cors = require("cors");
 // middleware setUp
 app.use(cors());
 app.use(express.json());
+// requiring dotenv
+require("dotenv").config();
 
 // root
 app.get("/", (req, res) => {
@@ -26,11 +28,18 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const userCollection = client.db("sms-snaps-db").collection("users");
+    const servicesCollection = client.db("sms-snaps-db").collection("services");
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
   } finally {
     // await client.close()
   }
 }
-run().catch((err) => console.log(err));
+run().catch(console.dir);
 
 app.listen(port, (req, res) => {
   console.log(`S.M.'s SnaP server is running at port${port}`);
